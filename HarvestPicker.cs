fcfdbaeba9ffc75510521a5ae288a6f45a86956d
bruno105 <bruno1057_@hotmail.com>
2024-08-14 22:18:20 -0300

    Added Index of CR on Map

--- HarvestPicker.cs
+++ HarvestPicker.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
@@ -16,6 +16,7 @@ using SharpDX;
 using Vector2 = System.Numerics.Vector2;
 using static MoreLinq.Extensions.PermutationsExtension;
 using System.Web;
+using ExileCore.Shared.Helpers;
 
 namespace HarvestPicker;
 
@@ -424,7 +425,7 @@ public class HarvestPicker : BaseSettingsPlugin<HarvestPickerSettings>
         {
             if (GameController.IngameState.IngameUi.Map.LargeMap.IsVisibleLocal)
             {
-               
+
                 var mapPos = GameController.IngameState.Data.GetGridMapScreenPosition(entity.PosNum.WorldToGrid());
                 mapPos = mapPos + new Vector2(0, 10);
                 if (text != null)
@@ -442,48 +443,24 @@ public class HarvestPicker : BaseSettingsPlugin<HarvestPickerSettings>
                 {
                     Graphics.DrawBox(new RectangleF(mapPos.X - size / 2, mapPos.Y - size / 2, size, size), color, 1f);
                 }
-                
+
             }
         }
 
- void DrawIndex(Entity entity, Color color, int size, string text)
- {
-     if (Settings.DrawRotationOnMap && GameController.IngameState.IngameUi.Map.LargeMap.IsVisibleLocal)
-     {
-        
-         var mapPos = GameController.IngameState.Data.GetGridMapScreenPosition(entity.PosNum.WorldToGrid());
-         mapPos = mapPos + new Vector2(0, 10);
-         if (text != null)
-         {
-             var widthPadding = 3;
-             var boxOffset = Graphics.MeasureText(text) / 2f;
-             var textOffset = boxOffset;
+        if (_cropRotationPath is { } path)
+        {
+            for (int i = 0; i < path.Count; i++)
+            {
+                var entity = path[i];
+                var text = $"CR: this is your choice index {i}. Total EV: {_cropRotationValue:F1}";
+                var textPosition = GameController.IngameState.Camera.WorldToScreen(entity.PosNum) + new Vector2(0, Graphics.MeasureText("V").Y);
+                Graphics.DrawBox(textPosition, textPosition + Graphics.MeasureText(text), Color.Black);
+                Graphics.DrawText(text, textPosition, i == 0 ? Settings.GoodColor : Settings.NeutralColor);
 
-             boxOffset.X += widthPadding;
+                DrawIndex(entity, i == 0 ? Settings.GoodColor : Settings.NeutralColor, 12, i.ToString());
+            }
+        }
 
-             Graphics.DrawBox(mapPos - boxOffset, mapPos + boxOffset, Color.Black);
-             Graphics.DrawText(text, mapPos - textOffset, color);
-         }
-         else
-         {
-             Graphics.DrawBox(new RectangleF(mapPos.X - size / 2, mapPos.Y - size / 2, size, size), color, 1f);
-         }
-         
-     }
- }
-
- if (_cropRotationPath is { } path)
- {
-     for (int i = 0; i < path.Count; i++)
-     {
-         var entity = path[i];
-         var text = $"CR: this is your choice index {i}. Total EV: {_cropRotationValue:F1}";
-         var textPosition = GameController.IngameState.Camera.WorldToScreen(entity.PosNum) + new Vector2(0, Graphics.MeasureText("V").Y);
-         Graphics.DrawBox(textPosition, textPosition + Graphics.MeasureText(text), Color.Black);
-         Graphics.DrawText(text, textPosition, i == 0 ? Settings.GoodColor : Settings.NeutralColor);
-
-         DrawIndex(entity, i == 0 ? Settings.GoodColor : Settings.NeutralColor, 12, i.ToString());
-     }
- }
     }
+
 }
